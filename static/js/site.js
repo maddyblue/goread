f783aead6b0eca99baf48678a512e8565d30a62b
Matt Jibson <matt.jibson@gmail.com>
2013-03-31 22:11:54 -0400

    CSS for current story

--- static/js/site.js
+++ static/js/site.js
@@ -132,8 +132,10 @@ function GoreadCtrl($scope, $http, $timeout) {
 		$scope.currentStory = i;
 		$scope.markRead(story);
 		$('#story' + i).html($scope.contents[story.guid] || '');
+		$('.story-header.selected').removeClass('selected');
 		setTimeout(function() {
 			se = $('#storydiv' + i);
+			$('.story-header', se).addClass('selected');
 			var docTop = $(window).scrollTop() + 40;
 			var docBottom = docTop + $(window).height() + 40;
 			var eTop = se.offset().top;
