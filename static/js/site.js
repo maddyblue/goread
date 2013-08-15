28db5375723a376142e61277b469a6772a91c744
Matt Jibson <matt.jibson@gmail.com>
2013-08-15 00:32:06 -0400

    Remove ugly base64 story URLs
    These were used to send correct referrers to google adsense.

--- static/js/site.js
+++ static/js/site.js
@@ -16,10 +16,8 @@ function countProperties(obj) {
 var goReadAppModule = angular.module('goReadApp', ['ui.sortable'])
 	.filter('encodeURI', function() {
 		return encodeURIComponent;
-	}).config(function($locationProvider) {
-		return $locationProvider.html5Mode(true);
 	});
-goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $window, $location) {
+goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $window) {
 	$scope.loading = 0;
 	$scope.contents = {};
 	$scope.opts = {
@@ -199,7 +197,6 @@ goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $wind
 		}
 		else if (!middleClick && !noClose && i == $scope.currentStory) {
 			delete $scope.currentStory;
-			$location.path('/');
 			return;
 		}
 		var story = $scope.dispStories[i];
@@ -228,7 +225,6 @@ goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $wind
 		if ($scope.opts.expanded || !$scope.storyCollapse) {
 			$scope.markAllRead(story);
 		}
-		$location.path('/s/' + Base64.encode(story.feed.XmlUrl) + '/' + Base64.encode(story.Id));
 	};
 
 	$scope.prev = function() {
@@ -496,7 +492,6 @@ goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $wind
 		$scope.updateUnreadCurrent();
 		$scope.resetScroll();
 		$scope.resetLimit();
-		$location.path('/');
 	};
 
 	$scope.setActiveFolder = function(folder) {
@@ -508,7 +503,6 @@ goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $wind
 		$scope.updateUnreadCurrent();
 		$scope.resetScroll();
 		$scope.resetLimit();
-		$location.path('/');
 	};
 
 	$scope.resetScroll = function() {
@@ -1115,64 +1109,3 @@ goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $wind
 		$scope.http('POST', $('#mark-all-read').attr('data-url'), { last: d.valueOf() });
 	};
 });
-
-// Modified from: http://stackoverflow.com/a/6740027/864236
-var Base64 = {
-	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=",
-
-	// public method for encoding
-	encode : function (input) {
-	var output = "";
-		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-		var i = 0;
-
-		input = Base64._utf8_encode(input);
-
-		while (i < input.length) {
-
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
-
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
-
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
-			}
-
-			output = output +
-				Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
-				Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
-
-		}
-		return output;
-	},
-
-	// private method for UTF-8 encoding
-	_utf8_encode : function (string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
-
-		for (var n = 0; n < string.length; n++) {
-			var c = string.charCodeAt(n);
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-		}
-		return utftext;
-	}
-}
