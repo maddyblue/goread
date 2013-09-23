8237ff37686e537699b4f174efa5e0ebb3700b64
Matt Jibson <matt.jibson@gmail.com>
2013-09-23 04:48:15 -0400

    Reduce mark read debounce

--- static/js/site.js
+++ static/js/site.js
@@ -388,7 +388,7 @@ goReadAppModule.controller('GoreadCtrl', function($scope, $http, $timeout, $wind
 			$http.post($('#mark-all-read').attr('data-url-read'), ss);
 			$scope.$apply();
 		}
-	}, 1000);
+	}, 250);
 
 	$scope.active = function() {
 		if ($scope.activeFolder) return $scope.activeFolder;
